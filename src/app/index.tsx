757bcfac15c89d0394f34a23acd2843407b8d8a9
Janak Dangi <janakdangi37@gmail.com>
2026-08-04 16:37:23 +0545

    Update app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,9 +5,9 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 export default function HomeScreen() {
   return (
    <view className="flex-1 items-center justify-center bg-white">
-    <Text className={"text-red-500"}>Open up App.js to start working on your app!</Text>
-    <StatusBar barStyle="dark-content" />
-   </view>
+    <Text >Uber Clone!</Text>
+    <StatusBar Style="auto" />
+   </SafeAreaView>
   );
 }
 
